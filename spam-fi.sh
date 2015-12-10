75a862775ca428f1413f0a778cd463571e93f193
Lee T <lee2sman@users.noreply.github.com>
2015-12-10 12:23:09 -0800

    added spam fish-script for terminal use

---
+++ spam-fi.sh
@@ -0,0 +1,5 @@
+function spam
+                        cd ~/Documents/code/p5/rita-test/
+                        python -m SimpleHTTPServer
+                        open http://127.0.0.1:8000/
+                    end
